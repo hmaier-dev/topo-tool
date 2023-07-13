2638221f7ca48e0a578a6b99f2f615b71348fdf5
hmaier <hmaier@ipb-halle.de>
2023-07-13 11:34:51 +0200

    changed dir structure

---
+++ discover.py
@@ -0,0 +1,45 @@
+import os.path
+# Package: napalm-h3c-comware
+from napalm import get_network_driver
+from data import cred
+
+'''
+Main Class where All the good Stuff Happens
+'''
+
+
+class Discovery:
+    def __init__(self):
+        self.USERNAME = cred.sw_user
+        self.PASSWORD = cred.sw_password
+        self.table_dir = "switch-tables"
+
+        if not os.path.exists(self.table_dir):
+            os.makedirs(self.table_dir)
+
+
+
+    def get_mac_table(self, switch):
+        """
+        Takes tuple with string: name and string: ip
+        """
+        name = switch[0]
+        ip = switch[1]
+        print(f"Connecting to {name} with {ip}...")
+        driver = get_network_driver("h3c_comware")
+        driver = driver(ip, self.USERNAME, self.PASSWORD)
+        driver.open()
+        print(f"Pulling MAC-Table from {name}...")
+        netconf_output = driver.get_mac_address_table()
+        # Write mac-address-table to a file
+        print(f"Formatting and writing to file...")
+        file = "RAW_" + name + ".json"
+        path = os.path.join(self.table_dir, file)
+        with open(path, "w") as out:
+            mac = str(netconf_output)
+            # Formatting the output-string to be useful for json
+            mac = mac.replace("\'", "\"")
+            mac = mac.replace(" True", "true")
+            mac = mac.replace(" False", "false")
+            out.write(mac)
+            out.close()
---
+++ filter.py
@@ -0,0 +1,64 @@
+import json
+import os
+import re
+
+'''
+This class should filter all unnecessary entrys as well, as sort the useful ones
+'''
+
+
+class Filtering:
+    def __init__(self, switch):
+        """
+        Logic is processed in the order of the following lists
+        """
+        self.noBridgeAggregation = []
+        self.withStackAndInterface = []
+        self.sortedByInterface = []
+
+        self.raw_file_name = "RAW_" + switch[0] + ".json"
+        self.clean_file_name = "CLEAN_" + switch[0] + ".json"
+        self.raw_file_ip = switch[1]
+
+        table_name = os.path.join("switch-tables", self.raw_file_name)
+        # Opening the desired file
+        with open(table_name, "r") as file:
+            self.raw_mac_table = json.loads(file.read())
+            file.close()
+
+    def cleaning_mac_table(self, regex_filter="Bridge-Aggregation"):
+        table = self.raw_mac_table
+        tmp_list = []
+        for entry in table:
+            mac = entry["mac"]
+            interface_name = entry["interface"]
+            vlan = entry["vlan"]
+            # Step 1
+            # Drop all non-local mac-addresses
+            if not re.match(regex_filter, interface_name):
+                # Step 2
+                # Resolving num of stack and interface
+                matches = re.findall(r'\d+', interface_name)
+                sort_helper = matches[0] + matches[2]  # combine two strings
+                add = {"mac": mac,
+                       "interface": interface_name,
+                       "vlan": vlan,
+                       "stack_num": matches[0],
+                       "interface_num": matches[2],
+                       "sort_helper": int(sort_helper)}
+                # Step 3
+                # Adding the new entry to a temporary list
+                tmp_list.append(add)
+            # Step 4
+            # returning the list sorted by the sort_helper key
+            return sorted(tmp_list, key=lambda x: (x["sort_helper"]))
+
+    def get_filtered_mac_table(self):
+        clean = self.cleaning_mac_table()
+
+        path = os.path.join("switch-tables", self.clean_file_name)
+        with open(path, "w") as out:
+            out.write(str(clean))
+            out.close()
+
+        # return clean
