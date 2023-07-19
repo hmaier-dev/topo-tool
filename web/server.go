6df4e581c715245c6deb544943ab7c3f4f8dcd7e
Hendrik Maier <hmaier@ipb-halle.de>
2023-07-19 18:42:34 +0200

    Minor: Playing around with golang as webserver alternative

--- web/server.go
+++ web/server.go
@@ -1,12 +1,32 @@
 package main
 
 import (
-  "fmt"
-  // "log"
-  // "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
-func main(){
-  fmt.Printf("Server at port 8080...\n")
+func main() {
+
+	// fileServer := http.FileServer(http.Dir("./static"))
+	// http.Handle("/", fileServer)
+	http.HandleFunc("/", indexHandler)
+	fmt.Printf("Server at port 8080...\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+	for _, element := range [100]int{} {
+		fmt.Fprintf(w, "%d", element)
+		time.Sleep(1000000)
+	}
 
 }
