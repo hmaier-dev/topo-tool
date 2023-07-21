2f8e252cec2f712bd2c385b2af3ce02899302358
Hendrik Maier <hmaier@ipb-halle.de>
2023-07-21 13:57:01 +0200

    Minor: Tinkering around, trying to get the database connection working

--- web/server.go
+++ web/server.go
@@ -8,55 +8,100 @@ import (
 )
 import _ "github.com/go-sql-driver/mysql"
 
+var conn *sql.DB
+
 func main() {
 
-	dbConnect()
+	var hostname = "azt-4816"
 
-	//// fileServer := http.FileServer(http.Dir("./static"))
-	//// http.Handle("/", fileServer)
-	//http.HandleFunc("/", indexHandler)
-	//fmt.Printf("Server at port 8080...\n")
-	//if err := http.ListenAndServe(":8080", nil); err != nil {
-	//	log.Fatal(err)
-	//}
-
+	conn = dbConnect()
+	var switchNames []string = switchTableNames()
+	for _, sw := range switchNames {
+		selectRowByHostname(sw, hostname)
+	}
+	conn.Close()
 }
 
-func dbConnect() {
+type switchRow struct {
+	id            int
+	interfaceName string
+	mac           string
+	hostname      string
+	vlan          int
+	stack         int
+	interfaceNum  int
+}
+
+func dbConnect() *sql.DB {
 	var user = "www-data"
 	var password = "password123"
 	var host = "localhost"
 	var port = "3306"
 	var dbName = "topology-tool"
 	var source = user + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbName
-
 	conn, err := sql.Open("mysql", source)
 	if err != nil {
 		fmt.Println("sql.open", err)
 		os.Exit(1)
 	}
+	return conn
+}
 
+func selectAllFromClearpass() {
 	query, err := conn.Prepare("SELECT *  FROM `clearpass`;")
 	rows, err := query.Query()
 	if err != nil {
 		fmt.Println("query.Query", err)
 	}
-	defer conn.Close()
-	colNames, err := rows.Columns()
-	colTypes, err := rows.ColumnTypes()
-	fmt.Println(colNames, colTypes)
-	for i, _ := range colTypes {
-		fmt.Println(colTypes[i].ScanType())
+	// iterator which yields the next row
+	for rows.Next() {
+		// declare variables to which Scan() can save the data to
+		var (
+			id       int
+			hostname string
+			mac      string
+		)
+		err := rows.Scan(&id, &hostname, &mac)
+		if err != nil {
+			return
+		}
+		fmt.Printf("%v, %v, %v \n", id, hostname, mac)
 	}
-
-	//for rows.Next() {
-	//	var id int16
-	//	rows.Scan(&id)
-	//	fmt.Println("resp", id)
-	//}
-
 }
 
+func selectRowByHostname(switchName string, hostname string) {
+	stmt, _ := conn.Prepare("SELECT id, interface_name, mac, hostname, vlan, stack , interface_num FROM `?` WHERE hostname = ? ;")
+	rows, err := stmt.Query(switchName, hostname)
+
+	if err == nil {
+		fmt.Println("stmt.Query", err)
+	}
+	//columns, _ := rows.ColumnTypes()
+	//response_array := make([]string, len(columns))
+	//iterator which yields the next row
+	for rows.Next() {
+		// declare variables to which Scan() can save the data to
+		var sr switchRow
+		rows.Scan(&sr.id, &sr.interfaceName, &sr.mac, &sr.hostname, &sr.vlan, &sr.stack, &sr.interfaceNum)
+		fmt.Printf("%v", sr)
+
+	}
+}
+
+func switchTableNames() []string {
+	query, _ := conn.Prepare("SHOW TABLES;")
+	rows, _ := query.Query()
+	columns, _ := rows.ColumnTypes()
+	switchNames := make([]string, len(columns))
+	var tableName string
+	for rows.Next() {
+		rows.Scan(&tableName)
+		if tableName != "clearpass" {
+			switchNames = append(switchNames, tableName)
+		}
+	}
+	return switchNames
+}
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, "Method is not supported.", http.StatusNotFound)
