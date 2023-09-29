15ecc5d935eec01737c9d4714cbf1d350d793b96
Hendrik Maier <hmaier@ipb-halle.de>
2023-09-29 11:50:08 +0200

    Minor: sorting logic and implement db function

--- data/main.go
+++ data/main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"bufio"
+	"database/sql" // you need to use a driver with this
 	"encoding/base64"
 	json2 "encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql" // this is the sql driver
 	"golang.org/x/crypto/ssh"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
 	"strings"
+	"time"
 )
 
+// SwitchInfo is used for the list containing the access switches
 type SwitchInfo struct {
 	Name string
 	IP   string
+}
+
+// SwitchData represents data from the ssh-output
+type SwitchData struct {
+	Mac       string `json:"mac-address"`
+	Vlan      string `json:"vlan"`
+	State     string `json:"state"`
+	Interface string `json:"interface"`
+	Stack     string `json:"stack"`
+	Port      string `json:"port"`
+	Aging     string `json:"aging"`
 }
 
 var switchesList = []SwitchInfo{
@@ -40,25 +55,64 @@ var switchesList = []SwitchInfo{
 	// {"SW_A121", "192.168.132.131"}, 1/0/24 set as uplink
 }
 
-type SwitchData struct {
-	Mac       string `json:"mac-address"`
-	Vlan      string `json:"vlan"`
-	State     string `json:"state"`
-	Interface string `json:"interface"`
-	Stack     string `json:"stack"`
-	Port      string `json:"port"`
-	Aging     string `json:"aging"`
+var conn *sql.DB
+
+func init() {
+	fmt.Println("Connecting to the db...")
+	conn = dbConnect()
 }
 
 func main() {
 	fmt.Println("Starting ssh connect...")
-	ssh_connector()
+	for {
+		query_clearpass()       // get all hostnames and ip-addresses
+		query_access_switches() //
+		time.Sleep(30 * time.Minute)
+	}
+}
+
+// Establishing the connection to the
+func dbConnect() *sql.DB {
+	var user = "www-data"
+	var password = "password123"
+	var host = "localhost"
+	//var host = "db"
+	var port = "3306"
+	var dbName = "topology-tool"
+	var source = user + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbName
+	timeout := 30
+	timeoutSec := 30 * time.Second
+	wait := 5
+	maxi := timeout / wait
+	try := 1
+	startTime := time.Now()
+	conn, err := sql.Open("mysql", source)
+	if err != nil {
+		log.Fatal("sql.open failed: ", err)
+	}
+
+	for {
+		err = conn.Ping()
+		if err != nil {
+			fmt.Printf("[%d/%d] Ping failed...\n", try, maxi)
+			try += 1
+		}
+		if err == nil {
+			fmt.Println("Connection to db succesful!")
+			break
+		}
+		if time.Since(startTime) >= timeoutSec {
+			log.Fatal("Timeout: No connection to db.")
+		}
+		time.Sleep(5 * time.Second)
+	}
+	return conn
 }
 
 // -----------------------------------------------------
-// SSH-Connector
-func ssh_connector() {
-
+//
+//	the connection to the access switches
+func query_access_switches() {
 	username, password := read_cread_from_file("access_switch", "./cred.txt")
 	config := &ssh.ClientConfig{
 		User: username,
@@ -72,7 +126,12 @@ func ssh_connector() {
 	if err != nil {
 		log.Fatalf("Failed to dial: %v", err)
 	}
-	defer conn.Close()
+	defer func(conn *ssh.Client) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalf("Error closing connection...")
+		}
+	}(conn)
 
 	// New session
 	session, err := conn.NewSession()
@@ -80,7 +139,8 @@ func ssh_connector() {
 		log.Fatalf("Failed to establish connection: %v", err)
 	}
 	raw := run_command(session, "display mac-address")
-	json := process_response(raw)
+	sorted := process_response(raw) // sort out non-significant interfaces and break up data
+	json := convertToJson(sorted)   // serialize into []byte
 
 	fmt.Printf("%v", string(json))
 
@@ -93,12 +153,13 @@ func run_command(session *ssh.Session, cmd string) string {
 	}
 	return string(out)
 }
-func process_response(dirty string) []byte {
+func process_response(dirty string) []SwitchData {
 	var macTable []SwitchData
 	// Explaining the regex:
 	// 								 MAC			  VLAN    State   Port    Aging
 	re := regexp.MustCompile(`([0-9a-fA-F-]{14})\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)`) // match the mac-address and the strings/whitespaces afterward
 	lines := strings.Split(dirty, "\n")                                            // building lines from
+	// Going through the whole ssh output!
 	for _, line := range lines {
 		if matches := re.FindStringSubmatch(line); matches != nil {
 			if !validate(matches) {
@@ -117,7 +178,10 @@ func process_response(dirty string) []byte {
 			macTable = append(macTable, data)
 		} // END if
 	} // END for
+	return macTable
+}
 
+func convertToJson(macTable []SwitchData) []byte {
 	// Build []byte with json
 	json, err := json2.Marshal(macTable)
 	if err != nil {
@@ -138,15 +202,17 @@ func disassembleInterfaceString(interfaceStr string) (string, string) {
 	re := regexp.MustCompile(`\d+`)                                // digits in interface string
 	if match := re.FindAllString(interfaceStr, -1); match != nil { // FindAllString with the -1 works, but I don't know why...
 		return match[0], match[2]
+	} else {
+		log.Fatalf("Could not disassemble: %s\n", interfaceStr)
+		return "", ""
 	}
-	return "", ""
 }
 
 //-----------------------------------------------------
 
 // --------------------------
 // CLEARPASS-CONNECTOR
-func clearpass() {
+func query_clearpass() {
 	var username, password string
 	username, password = read_cread_from_file("clearpass", "./cred.txt")
 	url := "http://clearpass-cl.ipb-halle.de/tipsapi/config/read/Endpoint"
